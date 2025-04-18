72e17f3b7f0732fb82dbc9e44ccb89ab0dd7a009
Cristopher Paiva <josuepaiva7@gmail.com>
2025-04-18 06:43:24 -0300

    fix: update contact email in footer

--- app/page.tsx
+++ app/page.tsx
@@ -48,7 +48,7 @@ export default function Home() {
           <div className="flex gap-4 items-center flex-col sm:flex-row mt-8">
             <a
               className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-              href="mailto:tu-email@ejemplo.com"
+              href="mailto:cristopher.paiva.dev@gmail.com"
               rel="noopener noreferrer"
             >
               Contáctame
